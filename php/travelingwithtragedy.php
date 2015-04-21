df8d8d5bcdb103b1d22926ff2382bd6b4dc573a1
James Ginns <starvagrant@yahoo.com>
2015-04-20 20:24:14 -0500

    Set up travelingwithtragedy .md, .html, and .php
    I've changed the traveling with tragedy in the markdown folder
    I've converted markdown into html in the html folder
    I've created a php folder for traveling with tragedy.php
    The php script takes GET urls located in the markdown

---
+++ php/travelingwithtragedy.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+	<meta charset="utf-8">
+<title></title>
+<style>
+/* {	text-indent: 4em;	} */
+</style>
+</head>
+
+<body>
+	<?php echo "<pre>"; var_dump($_GET); "</pre>"; ?>
+</body>
+</html>
