6087db8cf5bf40fe5d812d42c96fe11b51697b27
James Ginns <starvagrant@yahoo.com>
2015-07-29 19:19:13 -0500

    New Project: Theatre of Hades Website
    I've been planning an actual website for my Theatre of Hades
    web content. Here I have a two column layout and introductory
    material, with ideas for dynamically generated content. The
    layout looks like crap, but I'm satisfied with the color scheme.
    The CSS was a template, now heavily modified
    
    Original Source: http://codepen.io/meghanhoke/pen/Hjydp

---
+++ toh.php
@@ -0,0 +1,132 @@
+<?php
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>Theatre of Hades</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="generator" content="Geany 1.23.1" />
+	<link rel="stylesheet" type="text/css" href="toh.css" />
+</head>
+
+<body>
+
+<div class="container">
+<header>
+  <h1>Theatre of Hades</h1>
+</header>
+
+<nav>
+  <ul>
+	<li><a href="films.php">Films</a></li>
+	<li><a href="tickets.php">Tickets</a></li>
+	<li><a href="locations.php">Locations</a></li>
+	<li><a href="reviews.php">Reviews</a></li>
+	<li><a href="about.php">About Us</a></li>
+  </ul>
+</nav>
+
+<div class="main">
+  <div class="archives">
+	  <ul id="films-list">
+		<li><h4>Now Playing</h4></li>
+        <li><a href="films.php?film=spectreofatlantis">Spectre of Atlantis</a></li>
+		<li><a href="films.php?film=civilwarsandhospitalbeds">Civil Wars And Hosptial Beds</a></li>
+		<li><a href="films.php?film=roadtripinred">Road Trip in Red</a></li>
+		<li><a href="films.php?film=oozefromtheconfettifactory">Ooze from the Confetti Factory</a></li>
+		<li><a href="films.php?film=travelingwithtragedy">Traveling with Tragedy</a></li>
+		<li><a href="films.php?film=aneedleinistanbul">A Needle in Istanbul</a></li>
+		<li><a href="films.php?film=somethingviscousinstlouis">Something Viscous in St. Louis</a></li>
+		<li><a href="films.php?film=theculinaryblackarts">The Culinary Black Arts</a></li>
+		<li><a href="films.php?film=thenumerolgostcantstopthebees">The Numerlogist Can't Stop the Bees</a></li>
+		<li><a href="films.php?film=amousenamedmargot">A Mouse Named Margot</a></li>
+		<li><a href="films.php?film=theculinaryblackarts">The Culinary Black Arts</a></li>
+	</ul>
+	<ul id="calamity-list">
+		<li><h4>The Great Calamites</h4></li>	
+		<li><a href="calamities.php?atlantis">The Sinking of Atlantis</a></li>
+		<li><a href="calamities.php?egypt">The Origin of the Pyramids</a></li>
+		<li><a href="calamities.php?russia">The Collapse of the Ice Palace</a></li>
+		<li><a href="calamities.php?crete">Demolition of the Word Stones</a></li>
+		<li><a href="calamities.php?rome">Colosseum's Collapse</a></li>
+		<li><a href="calamities.php?aztec">Bare Sun on the Aztec Temple</a></li>
+		<li><a href="calamities.php?britishisles">The Tumbling Circles</a></li>
+		<li><a href="calamities.php?china">Breach of the Great Wall</a></li>
+		<li><a href="calamities.php?alexandria">Burning of the Great Library</a></li>
+		<li><a href="calamities.php?baghdad">Shattering the One Mosque</a></li>
+		<li><a href="calamities.php?greece">Death of the Oracles</a></li>
+		<li><a href="calamities.php?babylon">Dissolution of the Garden</a></li>
+	</ul>
+	<ul id="theatre-list">
+		<li><h4> The Theatres</h4></li>
+		<li><a href="theatres.php?prophets">Theatre of Prophets</a></li>
+		<li><a href="theatres.php?tragedy">Theatre of Tragedy</a></li>
+		<li><a href="theatres.php?trivia">Theatre of Trivia</a></li>
+		<li><a href="theatres.php?operations">Theatre of Operations</a></li>
+		<li><a href="theatres.php?eternity">Theatre of Eternity</a></li>
+		<li><a href="theatres.php?crystal">Theatre of Crystal</a></li>
+		<li><a href="theatres.php?curiosities">Theatre of Curiosities</a></li>
+		<li><a href="theatres.php?errata">Theatre of Errata</a></li>
+		<li><a href="theatres.php?speculation">Theatre of Speculation</a></li>
+		<li><a href="theatres.php?flora">Theatre of Flora</a></li>
+		<li><a href="theatres.php?abyss">Theatre of the Deep</a></li>
+		<li><a href="theatres.php?glory">Theatre of Glory</a></li>
+	</ul>
+
+  </div><!-- end of archives -->
+  <div class="news">
+	<div class="post">
+	<p class="quote"> 
+		You are entering the land of the dead. Nothing escapes the land of the dead.
+	</p>
+	<p>
+		Three Brothers, choose to split the rule of the world amongst them. Zeus the heavens, Poseidon the Sea, and Hades the Underworld. The sexual conquests of the first two are legendary (in its original meaning). One brother's sex life was not nearly
+so fulfilling. Happened upon a beautiful young girl (Kore, daughter of Demeter by Zeus) picking flowers. A hole opened from
+under the earth and he seized her.
+	</p>
+	<p>
+		Kore did not yield to his advances. Meanwhile, Hades sought the blessings of Zeus, her father. But her mother, Demeter, who had already been desperately searching for her daughter, was outraged at this request. As a goddess of harvest, for three years she let nothing grow. The populace, starving, had no offering to the gods, and Hades relented. 	
+	</p>
+	<p>
+		Or so it seemed. Those who eat the food of the underworld may not leave, and before leaving Kore ate four pomegrenate seeds.
+Whether she was tricked, starving, or ate them willingly to put an end to the conflict varies by the telling. But, for her consumption she was bound to the world four months, in which Demeter mourns and litte grows, and the surface world for spring in which the earth is reborn. 
+	</p>
+	<p>
+		Now, she has become Persephone, queen of the damned, as dread in her role under the earth as fruitful as she is below. She is the only being that regularly returns from the dead. Yet, from under the earth, Hades' domain, does all life flourish anew. The seeds of yesterday become the harvest of tomorrow. It too withers and dies, but not before casting out another seed. Thus is Hades complete acquistion (for he is the reprenstation of greed) negated. 
+	</p>
+	<p>
+		Time too, follows this pattern. For in each moment of time lies the seed for a new moment in time when the old moment passes away. And as our reality in time, so too does this hold for reality itself. But from what seeds does new reality sprout? What lays the ground for the future? Imagination. The creation of new realities always needs stories to work its way slowly, sometimes painfully, from the soil of the present. The story has been told orally in public and private. There has been the single story teller, there have been troupes from the theatre. Language has escaped the confines of sound and become the written word. In the recent era, the story has become the film. 
+	</p>
+	<p>
+		The Theatre of Hades, in some form, is as old as the story. It has only recently acquired that name. It is where reality is
+born, lives, suffers, and eventually dies. See, should you be so likely as to acquire a ticket, the birth of reality, and it's disintegration. 
+	</p>
+    </div>
+  </div><!-- end of news -->
+  
+</div><!-- end of main -->
+  <div class="footer">
+    <div class="footer-list">
+    <h4>List One</h4>
+    <ul>
+      <li><a href="#">We'll Be in Touch</a></li>
+      <li><a href="#">Ancient Documents Division</a></li>
+    </ul>
+    </div>
+    <div class="footer-list">
+    <h4>List Two</h4>
+    <ul>
+      <li><a href="#">Office of Proganda</a></li>
+      <li><a href="#">Center for Antiquities</a></li>
+    </ul>
+    </div>
+  </div>
+</div><!-- end of container -->	
+</body>
+
+</html>
+
